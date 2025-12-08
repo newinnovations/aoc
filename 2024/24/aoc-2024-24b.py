fb90fcb5420f0b00e9dcc9647cded223c780b13f
Martin van der Werff <github@newinnovations.nl>
2025-12-08 11:35:05 +0100

    updated 2024:24b

--- 2024/24/aoc-2024-24b.py
+++ 2024/24/aoc-2024-24b.py
@@ -1,82 +1,87 @@
 #!/usr/bin/env python3
 
-opers, swap, must_swap = dict(), dict(), list()
+opers, swap = dict(), dict()
 
 
-def find(var1, oper, var2):
+class NeedsSwap(Exception):
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+        super().__init__(f"Need to swap '{self.a}' and {self.b}")
+
+
+def find(in1, oper, in2):
     global opers, must_swap
-    for wire, (v1, op, v2) in opers.items():
-        if op == oper and ((var1 == v1 and var2 == v2) or (var1 == v2 and var2 == v1)):
-            return wire
-    if var2 and alternative(var1, oper):
-        must_swap = [var2, alternative(var1, oper)]
-    elif var1 and alternative(var2, oper):
-        must_swap = [var1, alternative(var2, oper)]
-    return None
+    for out, (i1, op, i2) in opers.items():
+        if op == oper and ((in1 == i1 and in2 == i2) or (in1 == i2 and in2 == i1)):
+            return out
+    if cp := swap_counterpart(in1, oper):
+        raise NeedsSwap(in2, cp)
+    elif cp := swap_counterpart(in2, oper):
+        raise NeedsSwap(in1, cp)
 
 
-def alternative(var, oper):
-    for _, (v1, op, v2) in opers.items():
+def swap_counterpart(input, oper):
+    for _, (i1, op, i2) in opers.items():
         if op == oper:
-            if v1 == var:
-                return v2
-            elif v2 == var:
-                return v1
-    return None
+            if i1 == input:
+                return i2
+            elif i2 == input:
+                return i1
+    return None  # should not happen
 
 
+# Try to build the adder device and check if the required operations on the wires are available
+#
+# If a required operation between two wires is not available it is because one of the wires was swapped. In this case we raise a NeedsSwap exception
+#
+# We find out which wire is swapped by looking for the requested operation on the other wire
+#
 # z00 ('y00', 'XOR', 'x00')
 # z01 (('x01', 'XOR', 'y01'), 'XOR', ('y00', 'AND', 'x00'))
 # z02 (('x02', 'XOR', 'y02'), 'XOR', (('x01', 'AND', 'y01'), 'OR', (('x01', 'XOR', 'y01'), 'AND', ('y00', 'AND', 'x00'))))
 # z03 (('x03', 'XOR', 'y03'), 'XOR', (('y02', 'AND', 'x02'), 'OR', (('x02', 'XOR', 'y02'), 'AND', (('x01', 'AND', 'y01'), 'OR', (('x01', 'XOR', 'y01'), 'AND', ('y00', 'AND', 'x00'))))))
-
-
-def is_device_working():
-    global opers, must_swap
-    opers.clear()
-    must_swap = []
+# and so on
+def check_device_is_adder():
+    global opers
     with open("input.txt") as f:
+        opers.clear()
         for line in f:
             if "-" in line:
                 var1, op, var2, _, out = line.strip().split(" ")
                 out = swap.get(out, out)
                 opers[out] = (var1, op, var2)
 
-    num_wires = sum([w[0] == "z" for w in opers.keys()])
+    num_outputs = sum([w[0] == "z" for w in opers.keys()])
 
     input_xor = dict()
-    for i in range(num_wires):
+    for i in range(num_outputs - 1):
         input_xor[i] = find(f"x{i:02}", "XOR", f"y{i:02}")
-        if must_swap:
-            return False
 
     input_and = dict()
-    for i in range(num_wires):
+    for i in range(num_outputs - 1):
         input_and[i] = find(f"x{i:02}", "AND", f"y{i:02}")
-        if must_swap:
-            return False
 
     carry, som = dict(), dict()
-    carry[1] = find("x00", "AND", "y00")
-    for i in range(2, num_wires):
-        and_term = find(input_xor[i - 1], "AND", carry[i - 1])
-        if must_swap:
-            return False
-        carry[i] = find(input_and[i - 1], "OR", and_term)
-        if must_swap:
-            return False
-        som[i] = find(input_xor[i], "XOR", carry[i])
-        if must_swap:
-            return False
-
-    return True
+    carry[0] = input_and[0]
+    som[0] = input_xor[0]
+    som[1] = find(input_xor[1], "XOR", carry[0])
+    for i in range(2, num_outputs):
+        and_term = find(input_xor[i - 1], "AND", carry[i - 2])
+        carry[i - 1] = find(input_and[i - 1], "OR", and_term)
+        if i == num_outputs - 1:
+            som[i] = carry[i - 1]
+        else:
+            som[i] = find(input_xor[i], "XOR", carry[i - 1])
 
 
 while True:
-    if is_device_working():
+    try:
+        check_device_is_adder()
         break
-    a, b = must_swap
-    swap[a] = b
-    swap[b] = a
+    except NeedsSwap as s:
+        print(s)
+        swap[s.a] = s.b
+        swap[s.b] = s.a
 
-print(",".join(sorted(swap.keys())))
+print(",".join(sorted(swap.keys())))  # cqr,ncd,nfj,qnw,vkg,z15,z20,z37
