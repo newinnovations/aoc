cf5fbc8ac8036a3f0cb8eefe9d829c4b338211e8
Martin van der Werff <github@newinnovations.nl>
2025-12-08 09:47:09 +0100

    2025:8

---
+++ 2025/8/aoc-2025-8b.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+
+import math
+from itertools import combinations
+
+filename = "input.txt"
+
+
+def distance(p1, p2):
+    x1, y1, z1 = p1
+    x2, y2, z2 = p2
+    return math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2 + (z2 - z1) ** 2)
+
+
+def circuit_for_box(box):
+    for i, circuit in enumerate(circuits):
+        if box in circuit:
+            return i
+    return -1  # should not happen
+
+
+boxes = list()
+with open(filename) as f:
+    for line in f:
+        x, y, z = map(int, line.strip().split(","))
+        boxes.append((x, y, z))
+
+circuits = [[box] for box in boxes]
+
+dists = [(distance(a, b), a, b) for a, b in combinations(boxes, 2)]
+dists = sorted(dists, key=lambda x: x[0])
+
+for _, a, b in dists:
+    ca = circuit_for_box(a)
+    cb = circuit_for_box(b)
+    if ca != cb:
+        circuits[ca] += circuits[cb]
+        del circuits[cb]
+    if len(circuits) == 1:
+        print(a[0] * b[0])  # 9253260633
+        break
