a1cc41014fd7bf372dca1bd44fda7d6465d2dc63
Martin van der Werff <github@newinnovations.nl>
2025-12-11 16:01:07 +0100

    updated 2024:11

--- 2024/11/aoc-2024-11b.py
+++ 2024/11/aoc-2024-11b.py
@@ -1,27 +1,24 @@
 #!/usr/bin/env python3
 
-with open("input.txt") as f:
-    pebbles = f.readline().strip().split(" ")
-
-lengths = dict()
+from functools import cache
 
 
+@cache
 def get_length(p, n):
     if n == 0:
         return 1
-    if (p, n) in lengths.keys():
-        return lengths[(p, n)]
     if p == "0":
-        res = get_length("1", n - 1)
-    elif (length := len(p)) % 2 == 0:
-        res = get_length(str(int(p[: length // 2])), n - 1) + get_length(
+        return get_length("1", n - 1)
+    if (length := len(p)) % 2 == 0:
+        return get_length(str(int(p[: length // 2])), n - 1) + get_length(
             str(int(p[length // 2 :])), n - 1
         )
-    else:
-        res = get_length(str(int(p) * 2024), n - 1)
-    lengths[(p, n)] = res
-    return res
+    return get_length(str(int(p) * 2024), n - 1)
 
 
-print(sum([get_length(p, 25) for p in pebbles]))  # 191690
+with open("input.txt") as f:
+    pebbles = f.readline().strip().split(" ")
+
+
+# print(sum([get_length(p, 25) for p in pebbles]))  # 191690
 print(sum([get_length(p, 75) for p in pebbles]))  # 228651922369703
